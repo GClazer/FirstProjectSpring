9e0a008d7a1c8f116cbf4995da20b2cade6e68b4
Gabriel H. Clazer <gabrielclazer.gc@hotmail.com>
2024-01-19 20:05:04 -0300

    Adicionando função para recuperar um unico registro do banco

--- src/main/java/com/clazerti/demo/resources/UserResource.java
+++ src/main/java/com/clazerti/demo/resources/UserResource.java
@@ -1,25 +1,32 @@
 package com.clazerti.demo.resources;
 
 import com.clazerti.demo.entities.User;
+import com.clazerti.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<ArrayList<User>> getAll() {
-        ArrayList<User> users = new ArrayList<>();
-        for (int i = 1; i < 5; i++) {
-            User user = new User(1L, "Gabriel " + i, "Clazer " + i, "gabriel@gmail.com " + i, "123");
-            users.add(user);
-        }
+    @Autowired
+    private UserService userService;
 
-        return ResponseEntity.ok().body(users);
+    @GetMapping("/get")
+    public ResponseEntity<List<User>> getAll() {
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
